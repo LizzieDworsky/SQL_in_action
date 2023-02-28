e5c1d19bfe0e10a07fc97952ccf65b53c48cafe9
LizzieDworsky <amydworsky15@gmail.com>
2023-02-28 14:19:16 -0700

    queries 1-9 done, 10-11 WIP

--- sql_in_action-starter.sql
+++ sql_in_action-starter.sql
@@ -2,14 +2,13 @@
 -- TODO: Remove the "--" from the below SELECT query and run the query
 --    NOTE: When writing queries, make sure each one ends with a semi-colon
 
--- SELECT * FROM final_airbnb;
-
-
+-- SELECT * FROM intro_sql.final_airbnb;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find out how many rows are in the table "final_airbnb"
 -- EXPECTED OUTPUT: 146
 
+-- SELECT count(id) FROM intro_sql.final_airbnb;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find out the name of the host for "host_id" 63613
@@ -17,6 +16,7 @@
 
 -- EXPECTED OUTPUT: Patricia
 
+-- SELECT host_name FROM intro_sql.final_airbnb WHERE host_id = 63613;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Query the data to just show the unique neighbourhoods listed
@@ -24,6 +24,7 @@
 
 -- EXPECTED OUTPUT: 40 neighbourhoods listed
 
+-- SELECT count(distinct neighbourhood) FROM intro_sql.final_airbnb;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 
@@ -34,6 +35,9 @@
 
 -- EXPECTED OUTPUT: Highest = 785, Lowest = 55
 
+-- SELECT * FROM intro_sql.final_airbnb WHERE price = (SELECT max(price) FROM intro_sql.final_airbnb);
+-- SELECT * FROM intro_sql.final_airbnb WHERE price = (SELECT min(price) FROM intro_sql.final_airbnb);
+-- SELECT * FROM intro_sql.final_airbnb WHERE price = (SELECT min(price) FROM intro_sql.final_airbnb) OR price = (SELECT max(price) FROM intro_sql.final_airbnb);
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the average availability for all listings in the data set (using the availability_365 column)
@@ -41,6 +45,7 @@
 
 -- EXPECTED OUTPUT: 165.3904
 
+-- SELECT avg(availability_365) FROM intro_sql.final_airbnb;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find all listings that do NOT have a review
@@ -48,6 +53,7 @@
 
 -- EXPECTED OUTPUT: 6 rows
 
+-- SELECT * FROM intro_sql.final_airbnb WHERE number_of_reviews = 0 or number_of_reviews = null;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 7 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the id of the listing with a room_type of "Private room" that has the most reviews 
@@ -55,6 +61,7 @@
 
 -- EXPECTED OUTPUT: 58059
 
+-- SELECT id FROM intro_sql.final_airbnb WHERE room_type = "Private room" ORDER BY number_of_reviews DESC LIMIT 1;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 8 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find the most popular neighbourhood for listings 
@@ -64,12 +71,15 @@
 -- EXPECTED OUTPUT: Williamsburg
 -- INVESTIGATE: Should Williamsburg be crowned the most popular neighbourhood?
 
+-- SELECT neighbourhood, count(neighbourhood) FROM intro_sql.final_airbnb group by neighbourhood order by count(neighbourhood) desc; 
+
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 9 >>>>>>>>>>>>>>>>>>>>>>>
 -- Query the data to discover which listing is the most popular using the reviews_per_month for all listings with a minimum_nights value of less than 7
 -- HINT: Sorting is still your friend! So are constraints.
 
 -- EXPECTED OUTPUT: 58059
 
+-- SELECT id FROM intro_sql.final_airbnb WHERE minimum_nights < 7 order by number_of_reviews desc limit 1;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 10 >>>>>>>>>>>>>>>>>>>>>>>
 -- Find out which host has the most listings. 
